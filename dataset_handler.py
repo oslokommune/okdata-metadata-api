2a87abcd8a3088181c20a0eb79e66a3e7c41a08a
Fredrik Vraalsen <fredrik@vraalsen.no>
2019-05-02 20:33:44 +0200

    Moved dataset ID generation to dataset repository

--- dataset_handler.py
+++ dataset_handler.py
@@ -1,7 +1,5 @@
 from difflib import SequenceMatcher
 
-import re
-import shortuuid
 import simplejson as json
 
 import common
@@ -11,13 +9,11 @@ import dataset_repository
 def post_dataset(event, context):
     """POST /datasets"""
 
-    body_as_json = json.loads(event["body"])
-    title_from_body = body_as_json["title"]
-    unique_id = generate_unique_id_based_on_title(title_from_body)
+    content = json.loads(event["body"])
 
-    db_response = dataset_repository.create_dataset(unique_id, body_as_json)
+    dataset_id = dataset_repository.create_dataset(content)
 
-    return common.response(200, unique_id)
+    return common.response(200, dataset_id)
 
 
 def update_dataset(event, context):
@@ -52,26 +48,6 @@ def get_dataset(event, context):
         return common.response(200, dataset)
     else:
         return common.response(404, "Selected dataset does not exist.")
-
-
-def generate_unique_id_based_on_title(title):
-    id = slugify(title)[:50]
-    if dataset_repository.dataset_exists(id):
-        return id + "-" + shortuuid.ShortUUID().random(length=5)
-    else:
-        return id
-
-
-def slugify(title):
-    a = 'àáäâãåăçèéëêæǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź_'
-    b = 'aaaaaaaceeeeeghiiiimnnnooooooprssstuuuuuwxyz '
-    tr = str.maketrans(a, b)
-    t = re.sub('\W+', '-', title.lower().translate(tr))
-    if t[0] == '-':
-        t = t[1:]
-    if t[-1] == '-':
-        t = t[0:-1]
-    return t
 
 
 def check_similarity_to_other_datasets(input_json):
--- dataset_repository.py
+++ dataset_repository.py
@@ -1,5 +1,7 @@
 import boto3
 from boto3.dynamodb.conditions import Key
+import re
+import shortuuid
 
 import common
 
@@ -15,32 +17,55 @@ def dataset_exists(dataset_id):
 
 
 def get_dataset(dataset_id):
-	db_response = dataset_table.query(
+    db_response = dataset_table.query(
         KeyConditionExpression=Key(common.DATASET_ID).eq(dataset_id)
     )
-	items = db_response["Items"]
+    items = db_response["Items"]
 
-	if len(items) == 0:
-		return None
-	elif len(items) > 1:
-		raise Exception(f"Illegal state: Multiple datasets with id {dataset_id}")
-	else:
-	    return items[0]
+    if len(items) == 0:
+        return None
+    elif len(items) > 1:
+        raise Exception(f"Illegal state: Multiple datasets with id {dataset_id}")
+    else:
+        return items[0]
 
 
 def get_datasets():
-	db_response = dataset_table.scan()
-	items = db_response["Items"]
-	return items
+    db_response = dataset_table.scan()
+    items = db_response["Items"]
+    return items
 
 
-def create_dataset(dataset_id, content):
-	content[common.DATASET_ID] = dataset_id
-	db_response = dataset_table.put_item(Item=content)
-	return db_response
+def create_dataset(content):
+    title = content["title"]
+    dataset_id = generate_unique_id_based_on_title(title)
+
+    content[common.DATASET_ID] = dataset_id
+    dataset_table.put_item(Item=content)
+    return dataset_id
 
 
 def update_dataset(dataset_id, content):
-	content[common.DATASET_ID] = dataset_id
-	db_response = dataset_table.put_item(Item=content)
-	return db_response
+    content[common.DATASET_ID] = dataset_id
+    db_response = dataset_table.put_item(Item=content)
+    return db_response
+
+
+def generate_unique_id_based_on_title(title):
+    id = slugify(title)[:50]
+    if dataset_exists(id):
+        return id + "-" + shortuuid.ShortUUID().random(length=5)
+    else:
+        return id
+
+
+def slugify(title):
+    a = 'àáäâãåăçèéëêæǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź_'
+    b = 'aaaaaaaceeeeeghiiiimnnnooooooprssstuuuuuwxyz '
+    tr = str.maketrans(a, b)
+    t = re.sub('\W+', '-', title.lower().translate(tr))
+    if t[0] == '-':
+        t = t[1:]
+    if t[-1] == '-':
+        t = t[0:-1]
+    return t
