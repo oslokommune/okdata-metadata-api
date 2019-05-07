c2941e0d2254d3415d54a994bf43e2f4d2596918
Fredrik Vraalsen <fredrik@vraalsen.no>
2019-05-08 00:04:14 +0200

    Remove unused stuff

--- tests/common_test_helper.py
+++ tests/common_test_helper.py
@@ -1,79 +1,11 @@
 from copy import deepcopy
-import json
 
 import common as table
-import dataset_handler
-import distribution_handler
-import edition_handler
-import version_handler
 
 dataset_table_name = "metadata-api-dataset"
 version_table_name = "metadata-api-version"
 edition_table_name = "metadata-api-edition"
 distribution_table_name = "metadata-api-distribution"
-
-
-def read_result_body(response):
-    return json.loads(response["body"])
-
-
-def post_dataset(*dataset_events):
-    return [dataset_handler.post_dataset(event, None) for event in dataset_events]
-
-
-def post_version(d_event, *version_events):
-    response_from_dataset_post = post_dataset(d_event)
-    response_from_dataset_post = response_from_dataset_post[0]
-    assert response_from_dataset_post["statusCode"] == 200
-
-    dataset_id = read_result_body(response_from_dataset_post)
-
-    version_responses = []
-    for v_event in version_events:
-        v_event["pathParameters"] = {"dataset-id": dataset_id}
-        version_responses.append(version_handler.post_version(v_event, None))
-
-    return response_from_dataset_post, version_responses
-
-
-def post_edition(d_event, v_event, *edition_events):
-    response_from_dataset_post, response_from_version_post = post_version(
-        d_event, v_event
-    )
-    response_from_version_post = response_from_version_post[0]
-    assert response_from_version_post["statusCode"] == 200
-
-    dataset_id = read_result_body(response_from_dataset_post)
-    version_id = read_result_body(response_from_version_post)
-
-    edition_responses = []
-    for e_event in edition_events:
-        e_event["pathParameters"] = {"version-id": version_id, "dataset-id": dataset_id}
-        edition_responses.append(edition_handler.post_edition(e_event, None))
-    return response_from_dataset_post, response_from_version_post, edition_responses
-
-
-def post_distribution(d_event, v_event, e_event, *distribution_events):
-    d_res, v_res, e_res = post_edition(d_event, v_event, e_event)
-    e_res = e_res[0]
-    assert e_res["statusCode"] == 200
-
-    dataset_id = read_result_body(d_res)
-    version_id = read_result_body(v_res)
-    edition_id = read_result_body(e_res)
-
-    distribution_responses = []
-    for distribution_event in distribution_events:
-        distribution_event["pathParameters"] = {
-            "version-id": version_id,
-            "dataset-id": dataset_id,
-            "edition-id": edition_id,
-        }
-        distribution_responses.append(
-            distribution_handler.post_distribution(distribution_event, None)
-        )
-
-    return d_res, v_res, e_res, distribution_responses
 
 
 def create_dataset_table(dynamodb):
@@ -159,8 +91,6 @@ dataset_updated = {
     "publisher": "REN",
 }
 
-dataset_event = {"body": json.dumps(dataset)}
-
 version = {
     "datasetID": "antall-besokende-pa-gjenbruksstasjoner",
     "versionID": "6-TEST",
@@ -178,8 +108,6 @@ version_updated = {
     "schema": {},
     "transformation": {},
 }
-
-version_event = {"body": json.dumps(version)}
 
 edition = {
     "datasetID": "antall-besokende-pa-gjenbruksstasjoner",
@@ -200,8 +128,6 @@ edition_updated = {
     "startTime": "2018-12-21T08:00:00+01:00",
     "endTime": "2018-12-21T09:00:00+01:00",
 }
-
-edition_event = {"body": json.dumps(edition)}
 
 distribution = {
     "datasetID": "antall-besokende-pa-gjenbruksstasjoner",
@@ -224,5 +150,3 @@ distribution_updated = {
     "format": "text/csv",
     "checksum": "...",
 }
-
-distribution_event = {"body": json.dumps(distribution)}
