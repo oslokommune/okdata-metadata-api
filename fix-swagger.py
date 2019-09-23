a575a1c49404bf6155740394c885a87af3018c8e
torklo <tord.kloster@knowit.no>
2019-09-23 14:37:17 +0200

    DP-12 add extra info, ignored by the plugin

--- fix-swagger.py
+++ fix-swagger.py
@@ -1,11 +1,15 @@
 import yaml
 
-
 """
     There are some issues with the serverless plugin adding the options (cors) endpoints to the swagger spec,
     without using correct path parameters etc. This clears the options endpoints from the swagger file, as they are
     generally not needed / implicit. 
+    
+    
+    This also adds the extra info properties that are not working (also a serverless plugin problem)
+    
 """
+
 
 def filterOptions(path):
     if "options" in path:
@@ -13,12 +17,23 @@ def filterOptions(path):
     return path
 
 
+def addInfo(contents):
+    contents["info"]["description"] = """Api for å registrere og redigere metadata for datasett i dataplattformen. 
+    Create og Update endepunktene krever autorisasjon, mens resten er åpent. """
+    contents["info"]["contact"] = {
+        "name": "Origo dataplattform",
+        "email": "dataplattform@oslo.kommune.no"
+    }
+    return contents
+
+
 def read():
     with open("swagger.yaml", "r") as f:
         swagger = yaml.safe_load(f)
         paths = swagger['paths']
-        swagger['paths'] = {name:filterOptions(paths[name]) for name in paths}
-        return swagger
+        swagger['paths'] = {name: filterOptions(paths[name]) for name in paths}
+        return addInfo(swagger)
+
 
 def write(content):
     with open("swagger.yaml", "w") as f:
@@ -26,4 +41,4 @@ def write(content):
 
 
 fixed = read()
-write(yaml.dump(fixed, sort_keys=False))
+write(yaml.dump(fixed, sort_keys=False, allow_unicode=True))
