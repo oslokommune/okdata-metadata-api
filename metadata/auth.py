0abae479c2486d53f7860806edeee0a669f12aab
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2021-02-02 10:24:20 +0100

    DP-1693: Fix previous commit

--- metadata/auth.py
+++ metadata/auth.py
@@ -2,12 +2,6 @@ import os
 import requests
 from keycloak import KeycloakOpenID
 
-AUTHORIZER_API = os.environ["AUTHORIZER_API"]
-KEYCLOAK_SERVER = "{}/auth/".format(os.environ["KEYCLOAK_SERVER"])
-KEYCLOAK_REALM = os.environ.get("KEYCLOAK_REALM", "api-catalog")
-CLIENT_ID = os.environ["CLIENT_ID"]
-CLIENT_SECRET = os.environ["CLIENT_SECRET"]
-
 
 class Auth:
     def __init__(self):
@@ -19,10 +13,10 @@ class Auth:
 
     def service_client_authorization_header(self):
         client = KeycloakOpenID(
-            server_url=KEYCLOAK_SERVER,
-            realm_name=KEYCLOAK_REALM,
-            client_id=CLIENT_ID,
-            client_secret_key=CLIENT_SECRET,
+            server_url=self.KEYCLOAK_SERVER,
+            realm_name=self.KEYCLOAK_REALM,
+            client_id=self.CLIENT_ID,
+            client_secret_key=self.CLIENT_SECRET,
         )
         response = client.token(grant_type=["client_credentials"])
         access_token = f"{response['token_type']} {response['access_token']}"
@@ -30,19 +24,9 @@ class Auth:
 
     def is_dataset_owner(self, token, dataset_id):
         result = requests.get(
-            f"{AUTHORIZER_API}/{dataset_id}",
+            f"{self.AUTHORIZER_API}/{dataset_id}",
             headers={"Authorization": f"Bearer {token}"},
         )
         result.raise_for_status()
         data = result.json()
         return data.get("access", False)
-
-
-def is_dataset_owner(token, dataset_id):
-    result = requests.get(
-        f"{AUTHORIZER_API}/{dataset_id}",
-        headers={"Authorization": f"Bearer {token}"},
-    )
-    result.raise_for_status()
-    data = result.json()
-    return data.get("access", False)
