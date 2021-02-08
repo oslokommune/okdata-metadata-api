7728392971e3b37dba695c558a9789ad1ba8f8c5
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2021-02-08 15:30:37 +0100

    DP-1349: Add script for setting source.type='file' on existing datasets

--- scripts/SCRIPTS.md
+++ scripts/SCRIPTS.md
@@ -2,9 +2,12 @@ Metadata Scripts
 ===================
 
 Python scripts for enriching and migrating metadata. Files in this directory are NOT supposed to be included in deployment.
-Purpose of writing scripts here is to utilize existing code in the metadata module.
+Purpose of writing scripts here is to utilize existing code in the metadata module. Puspose of keeping the scripts, although they typically only need to be run once,
+is for traceability.
 
 ## Scripts
 
 * set_event_source
     * This script was used to set source.type = event for all datasets with event streams.
+* set_file_source 
+    * This script was used to set source.type = file for all file based datasets.
---
+++ scripts/set_file_source.py
@@ -0,0 +1,28 @@
+import os
+from metadata.dataset.repository import DatasetRepository
+from aws_xray_sdk.core import xray_recorder
+
+xray_recorder.begin_segment("Set-event-source-script")
+
+env = "dev"
+
+# At this point in time source.type for all event datasets are set to 'event'
+# and source.type for all database sourced datasets (bym-geodata) are set to 'database'
+# This means that all datasets where source is not set can be set to source.type='file'
+
+if __name__ == "__main__":
+    if env == "dev":
+        os.environ["AWS_PROFILE"] = "okdata-dev"
+    elif env == "prod":
+        os.environ["AWS_PROFILE"] = "okdata-prod"
+    else:
+        raise Exception(f"Invalid env {env}")
+
+    dataset_repository = DatasetRepository()
+
+    all_datasets = dataset_repository.get_datasets()
+
+    for dataset in all_datasets:
+        if "source" not in dataset:
+            dataset_id = dataset["Id"]
+            dataset_repository.patch_item(dataset_id, {"source": {"type": "file"}})
