e2442f3a909061ebfec8071cdbdbe0e1e3667a4f
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2021-02-10 15:59:17 +0100

    DP-1745: Use argparse for env

--- scripts/set_null_source.py
+++ scripts/set_null_source.py
@@ -1,21 +1,19 @@
+import argparse
 import os
 from metadata.dataset.repository import DatasetRepository
 from aws_xray_sdk.core import xray_recorder
 
 xray_recorder.begin_segment("Set-file-source-script")
 
-env = "dev"
-
 # For all datasets checks for parent_id and adds parent_id to a list
 # Sets source=null for all datasets with id=parant_id
 
 if __name__ == "__main__":
-    if env == "dev":
-        os.environ["AWS_PROFILE"] = "okdata-dev"
-    elif env == "prod":
-        os.environ["AWS_PROFILE"] = "okdata-prod"
-    else:
-        raise Exception(f"Invalid env {env}")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", required=True, choices=["dev", "prod"])
+    args = parser.parse_args()
+
+    os.environ["AWS_PROFILE"] = f"okdata-{args.env}"
 
     dataset_repository = DatasetRepository()
 
