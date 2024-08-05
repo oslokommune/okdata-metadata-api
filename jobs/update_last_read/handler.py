216d9bf53539ecae2dc0623ef65fef655c407725
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2024-08-05 14:12:09 +0200

    Do some more logging

--- jobs/update_last_read/handler.py
+++ jobs/update_last_read/handler.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import datetime, timedelta
+from operator import itemgetter
 
 import boto3
 from aws_xray_sdk.core import patch_all, xray_recorder
@@ -52,6 +53,26 @@ def handler(event, context):
                 if dataset_id not in datasets_read or datasets_read[dataset_id] < dt:
                     datasets_read[dataset_id] = dt
 
+    updated_datasets = []
+    not_found_datasets = []
+
     for dataset, dt in datasets_read.items():
         if dataset_repository.dataset_exists(dataset):
             dataset_repository.patch_dataset(dataset, {"last_read": dt.isoformat()})
+            updated_datasets.append((dataset, dt))
+        else:
+            not_found_datasets.append(dataset)
+
+    logger.info("Updated datasets:")
+    if updated_datasets:
+        for dataset, dt in sorted(updated_datasets, key=itemgetter(0)):
+            logger.info(f" - {dataset}: {dt}")
+    else:
+        logger.info(" None")
+
+    logger.info("Not found datasets:")
+    if not_found_datasets:
+        for dataset in sorted(not_found_datasets):
+            logger.info(f" - {dataset}")
+    else:
+        logger.info(" None")
